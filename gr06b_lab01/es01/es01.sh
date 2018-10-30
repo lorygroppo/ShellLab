24d4cda403f1f9fab335ea121fc55331bb161b0e
lorygroppo <44590916+lorygroppo@users.noreply.github.com>
2018-10-30 22:53:25 +0100

    Add files via upload

---
+++ gr06b_lab01/es01/es01.sh
@@ -0,0 +1,75 @@
+case $1 in
+	1)
+		if (( $#==2 ))
+		then
+			find ~ -name $2 -type f -print
+		else
+			echo "ERRORE: numero di parametri errato!"
+		fi
+		;;
+	2)
+		if (( $#==3 ))
+		then
+			name=$2;
+			if [ ! -d $3 ]
+			then
+				mkdir $3
+				echo "La cartella non esisteva ed è stata creata"
+			fi
+			cd $3
+			if [ -e $2 ]
+			then
+				echo ERRORE: il file $2 esiste già
+			else
+				`touch $2`
+			fi
+		else
+			echo "ERRORE: numero di paramentri errato!"
+		fi
+		;;
+	3)
+		if (( $#==4 ))
+		then
+			if [ -d $3 ]
+			then
+				if [ -d $4 ]
+				then
+					mv $3/$2 $4
+				else
+					echo "cartella non esistente, creare la cartella? (Y/n)"
+					read risp
+					if(( $risp == "Y" ))
+					then
+						mkdir $4
+						mv $3/$2 $4
+					else
+						echo Cartella non creata
+					fi		 
+				fi 
+			else
+				echo "cartella sorgente inesistente"
+			fi 
+		else
+			echo "numero di parametri errato"
+		fi
+	;;
+	4)
+		if(( $#==2))
+		then
+			rm $2
+		else
+			echo "numero di parametri errato"
+		fi
+	;;
+	5)
+		if(( $#==2))
+		then
+			rm -r $2
+		else
+			echo "numero di parametri errato"
+		fi
+	;;
+	*)
+		echo "comando errato o assente"
+	;;
+esac
---
+++ gr06b_lab01/es02/es02.sh
@@ -0,0 +1,12 @@
+exec 3<host.txt
+exec 4>hostRenamed.txt
+while read LINE <&3
+do
+	m=$( echo "$LINE" | cut -d ')' -f 1 )
+	n=$(( $m+30 ))
+	ip="192.168.1.$n"
+	dominio=$(echo "$LINE" | cut -d ' ' -f 3 )
+	host="host$m"
+	dominio="$host.${dominio#*.}"
+	echo "$ip $dominio $host" >&4
+done
