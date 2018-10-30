ac7d1df1d24db7cb4e2c54d431135e77c84d41c2
lorygroppo <44590916+lorygroppo@users.noreply.github.com>
2018-10-30 16:51:18 +0100

    Update es01.sh

--- lab1/es01/es01.sh
+++ lab1/es01/es01.sh
@@ -1,15 +1,32 @@
 case $1 in
 	1)
-		if(($#!=2))
+		if (( $#==2 ))
 		then
-			echo "numero di parametri errato"
+			find ~ -name $2 -type f -print
 		else
-			find  $2
+			echo "ERRORE: numero di parametri errato!"
 		fi
-	;;
+		;;
 	2)
-
-	;;
+		if (( $#==3 ))
+		then
+			name=$2;
+			if [ ! -d $3 ]
+			then
+				mkdir $3
+				echo "La cartella non esisteva ed è stata creata"
+			fi
+			cd $3
+			if [ -e $2 ]
+			then
+				echo ERRORE: il file $2 esiste già
+			else
+				`touch $2`
+			fi
+		else
+			echo "ERRORE: numero di paramentri errato!"
+		fi
+		;;
 	3)
 		if(( $#==4))
 		then
