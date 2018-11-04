33fda986e43d50aca4e2866a95783fef98cb1b05
lorygroppo <44590916+lorygroppo@users.noreply.github.com>
2018-11-04 07:25:52 +0100

    Update es02.sh

--- gr06b_lab01/es02/es02.sh
+++ gr06b_lab01/es02/es02.sh
@@ -3,10 +3,12 @@ exec 4>hostRenamed.txt
 while read LINE <&3
 do
 	m=$( echo "$LINE" | cut -d ')' -f 1 )
+	parte 1
 	n=$(( $m+30 ))
 	ip="192.168.1.$n"
 	dominio=$(echo "$LINE" | cut -d ' ' -f 3 )
 	host="host$m"
 	dominio="$host.${dominio#*.}"
 	echo "$ip $dominio $host" >&4
+	parte 2
 done
